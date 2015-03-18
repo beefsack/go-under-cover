79662c82a1b2d15fc8aec8002dffd319ad0d9f9f
Michael Alexander <beefsack@gmail.com>
2015-03-18 22:22:51 +0800

    Refactored bridge to use io.ReadWriter instead of connections.

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,53 +1,24 @@
 package bridge
 
 import (
-	"fmt"
 	"io"
-	"net"
-	"sync"
-	"time"
 
 	"github.com/beefsack/go-under-cover/llog"
 )
 
-func Bridge(c1, c2 net.Conn) {
-	bridgeStr := fmt.Sprintf("%s <-> %s", c1.RemoteAddr(), c2.RemoteAddr())
-	llog.Trace("%s : bridging", bridgeStr)
-	quit := make(chan struct{}, 2)
-	wg := sync.WaitGroup{}
-	wg.Add(2)
+func Bridge(rw1, rw2 io.ReadWriter) error {
+	var err error
+	quit := make(chan struct{}, 1)
 	go func() {
-		ConnCopy(c1, c2, quit)
+		_, err = io.Copy(rw1, rw2)
 		quit <- struct{}{}
-		wg.Done()
 	}()
 	go func() {
-		ConnCopy(c2, c1, quit)
+		_, err = io.Copy(rw2, rw1)
 		quit <- struct{}{}
-		wg.Done()
 	}()
-	wg.Wait()
-	llog.Trace("%s : finished", bridgeStr)
-}
-
-func ConnCopy(dst, src net.Conn, quit chan struct{}) error {
-	copyStr := fmt.Sprintf("%s <- %s", dst.RemoteAddr(), src.RemoteAddr())
-	llog.Trace("%s : starting", copyStr)
-	for {
-		select {
-		case <-quit:
-			llog.Trace("%s : quitting", copyStr)
-			return nil
-		default:
-		}
-		dl := time.Now().Add(time.Second)
-		src.SetDeadline(dl)
-		dst.SetDeadline(dl)
-		_, err := io.Copy(dst, src)
-		netErr, ok := err.(net.Error)
-		if err == nil || !ok || !netErr.Timeout() {
-			llog.Debug("%s : ended with error %v", copyStr, err)
-			return err
-		}
-	}
+	llog.Trace("waiting for quit")
+	<-quit
+	llog.Trace("quit")
+	return err
 }
